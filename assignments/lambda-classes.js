85cded585caac222cead9a0462a150cae04d5441
Rodney-Race <rodneyrace@gmail.com>
2019-08-15 23:03:19 -0400

    update

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,1 +1,23 @@
 // CODE here for your Lambda Classes
+//Person Class/Base class
+class Person {
+    baseClass(name, age, location) {
+        this.name = name;
+        this.age = age;
+        this.location = location;
+    }
+    speak() {
+        console.log(`Hello my name is ${this.name}, I am from ${this.location}`);
+      }
+}
+
+//Instructor Class
+class Instructor extends Person {
+    constructor(name, age, location, specialty, favLanguage, catchPhrase) {
+        super(name, age, location);
+        this.specialty = specialty;
+        this.favLanguage = language;
+        this.catchPhrase = catchPhrase;
+    }
+    
+}
--- assignments/prototype-refactor.js
+++ assignments/prototype-refactor.js
@@ -1,9 +1,95 @@
-/* 
+//Prototype Refactor
+//1. Copy and paste your code or the solution from yesterday
+  //=== GameObject ===
+  function GameObject(properties) {
+    this.createdAt = properties.createdAt;
+    this.name = properties.name;
+    this.dimensions = properties.dimensions;
+  GameObject.prototype.destroy = function () {
+    return `${this.name} was removed from the game.`;
+   };
+  }
 
-Prototype Refactor
+  // === CharacterStats ===
+  function CharacterStats(attributes) {
+    GameObject.call(this, attributes);
+    this.healthPoints = attributes.healthPoints;
+  CharacterStats.prototype.takeDamage = function () {
+    return `${this.name} took damage.`;
+   };
+  }
+  CharacterStats.prototype = Object.create(GameObject.prototype);
+ 
+  // === Humanoid (Having an appearance or character resembling that of a human.) ===
+  function Humanoid (attributes) {
+    this.team = attributes.team;
+    this.weapons = attributes.weapons;
+    this.language = attributes.language;
+    Humanoid.prototype.greet = function() {
+      return `${this.name} offers a greeting in ${this.language}`;
+   };
+  }
+  Humanoid.prototype = Object.create(CharacterStats.prototype);
 
-1. Copy and paste your code or the solution from yesterday
+  const mage = new Humanoid({
+    createdAt: new Date(),
+    dimensions: {
+      length: 2,
+      width: 1,
+      height: 1,
+    },
+    healthPoints: 5,
+    name: 'Bruce',
+    team: 'Mage Guild',
+    weapons: [
+      'Staff of Shamalama',
+    ],
+    language: 'Common Tongue',
+  });
 
-2. Your goal is to refactor all of this code to use ES6 Classes. The console.log() statements should still return what is expected of them.
+  const swordsman = new Humanoid({
+    createdAt: new Date(),
+    dimensions: {
+      length: 2,
+      width: 2,
+      height: 2,
+    },
+    healthPoints: 15,
+    name: 'Sir Mustachio',
+    team: 'The Round Table',
+    weapons: [
+      'Giant Sword',
+      'Shield',
+    ],
+    language: 'Common Tongue',
+  });
 
-*/
+  const archer = new Humanoid({
+    createdAt: new Date(),
+    dimensions: {
+      length: 1,
+      width: 2,
+      height: 4,
+    },
+    healthPoints: 10,
+    name: 'Lilith',
+    team: 'Forest Kingdom',
+    weapons: [
+      'Bow',
+      'Dagger',
+    ],
+    language: 'Elvish',
+  });
+
+  console.log(mage.createdAt); // Today's date
+  console.log(archer.dimensions); // { length: 1, width: 2, height: 4 }
+  console.log(swordsman.healthPoints); // 15
+  console.log(mage.name); // Bruce
+  console.log(swordsman.team); // The Round Table
+  console.log(mage.weapons); // Staff of Shamalama
+  console.log(archer.language); // Elvish
+  console.log(archer.greet()); // Lilith offers a greeting in Elvish.
+  console.log(mage.takeDamage()); // Bruce took damage.
+  console.log(swordsman.destroy()); // Sir Mustachio was removed from the game.
+
+//2. Your goal is to refactor all of this code to use ES6 Classes. The console.log() statements should still return what is expected of them.
